c901d3841e8cf2ee857e3434280558f99c1afcc8
SF <smfoit4311@gmail.com>
2020-02-14 21:24:55 -0600

    Turn Based Combat
    Change combat

---
+++ Assets/Code/Unit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int health;
+    public int strength;
+    public int defense;
+}
---
+++ Assets/Code/UnitHUD.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHUD : MonoBehaviour
+{
+    public SliderJoint2D hpSlider;
+
+    
+}
