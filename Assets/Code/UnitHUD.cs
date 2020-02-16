e2be217f475fff9516817938ff9a36d11b0c7638
SF <smfoit4311@gmail.com>
2020-02-16 07:27:43 -0600

    Health text no longer goes below 0

--- Assets/Code/UnitHUD.cs
+++ Assets/Code/UnitHUD.cs
@@ -20,10 +20,18 @@ public class UnitHUD : MonoBehaviour
         hpSlider.maxValue = PlayerUnit.maxHealth;
         hpSlider.value = PlayerUnit.currentHealth;
 
-        playerHpText.text = "   " + PlayerUnit.currentHealth + " / " + PlayerUnit.maxHealth;
         strengthText.text = ": " + PlayerUnit.strength;
         defenseText.text = ": " + PlayerUnit.defense;
         speedText.text = ": " + PlayerUnit.speed;
+
+        if (PlayerUnit.currentHealth <= 0)
+        {
+            playerHpText.text = "   0 / " + PlayerUnit.maxHealth;
+        }
+        else
+        {
+            playerHpText.text = "   " + PlayerUnit.currentHealth + " / " + PlayerUnit.maxHealth;
+        }
     }
 
     public void SetHUD(EnemyUnit enemyUnit)
@@ -31,11 +39,20 @@ public class UnitHUD : MonoBehaviour
         hpSlider.maxValue = EnemyUnit.maxHealth;
         hpSlider.value = EnemyUnit.currentHealth;
 
-        enemyHpText.text = "Hp " + EnemyUnit.currentHealth + " / " + EnemyUnit.maxHealth;
         strengthText.text = ": " + EnemyUnit.strength;
         defenseText.text = ": " + EnemyUnit.defense;
         speedText.text = ": " + EnemyUnit.speed;
+
+        if (EnemyUnit.currentHealth <= 0)
+        {
+            enemyHpText.text = "   0 / " + EnemyUnit.maxHealth;
+        }
+        else
+        {
+            enemyHpText.text = "   " + EnemyUnit.currentHealth + " / " + EnemyUnit.maxHealth;
+        }
     }
+    
 
     public void SetHp(int hp)
     {
